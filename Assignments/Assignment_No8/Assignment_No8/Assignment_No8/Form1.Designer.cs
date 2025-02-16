3c91395f1b09c2d6d77eeb9c616aebf403a3702c
Vaishnavi-224 <vaishuraut2003@gmail.com>
2025-02-16 13:26:30 +0530

    Assignment

---
+++ Assignments/Assignment_No8/Assignment_No8/Assignment_No8/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Assignment_No8
+{
+    partial class frm_Dependant_Combo_Box
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Category = new System.Windows.Forms.Label();
+            this.lbl_Subcategory = new System.Windows.Forms.Label();
+            this.lbl_Product = new System.Windows.Forms.Label();
+            this.lbl_Quantity = new System.Windows.Forms.Label();
+            this.lbl_CS = new System.Windows.Forms.Label();
+            this.lbl_Total = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Category
+            // 
+            this.lbl_Category.AutoSize = true;
+            this.lbl_Category.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Category.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_Category.Location = new System.Drawing.Point(115, 190);
+            this.lbl_Category.Name = "lbl_Category";
+            this.lbl_Category.Size = new System.Drawing.Size(126, 34);
+            this.lbl_Category.TabIndex = 0;
+            this.lbl_Category.Text = "Category";
+            
+            // 
+            // lbl_Subcategory
+            // 
+            this.lbl_Subcategory.AutoSize = true;
+            this.lbl_Subcategory.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Subcategory.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_Subcategory.Location = new System.Drawing.Point(464, 190);
+            this.lbl_Subcategory.Name = "lbl_Subcategory";
+            this.lbl_Subcategory.Size = new System.Drawing.Size(170, 34);
+            this.lbl_Subcategory.TabIndex = 1;
+            this.lbl_Subcategory.Text = "Subcategory";
+            // 
+            // lbl_Product
+            // 
+            this.lbl_Product.AutoSize = true;
+            this.lbl_Product.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Product.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_Product.Location = new System.Drawing.Point(871, 190);
+            this.lbl_Product.Name = "lbl_Product";
+            this.lbl_Product.Size = new System.Drawing.Size(119, 34);
+            this.lbl_Product.TabIndex = 2;
+            this.lbl_Product.Text = "Product";
+            
+            // 
+            // lbl_Quantity
+            // 
+            this.lbl_Quantity.AutoSize = true;
+            this.lbl_Quantity.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Quantity.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_Quantity.Location = new System.Drawing.Point(101, 396);
+            this.lbl_Quantity.Name = "lbl_Quantity";
+            this.lbl_Quantity.Size = new System.Drawing.Size(129, 34);
+            this.lbl_Quantity.TabIndex = 3;
+            this.lbl_Quantity.Text = "Quantity";
+            // 
+            // lbl_CS
+            // 
+            this.lbl_CS.AutoSize = true;
+            this.lbl_CS.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_CS.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_CS.Location = new System.Drawing.Point(518, 396);
+            this.lbl_CS.Name = "lbl_CS";
+            this.lbl_CS.Size = new System.Drawing.Size(50, 34);
+            this.lbl_CS.TabIndex = 4;
+            this.lbl_CS.Text = "CS";
+            
+            // 
+            // lbl_Total
+            // 
+            this.lbl_Total.AutoSize = true;
+            this.lbl_Total.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Total.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.lbl_Total.Location = new System.Drawing.Point(883, 396);
+            this.lbl_Total.Name = "lbl_Total";
+            this.lbl_Total.Size = new System.Drawing.Size(84, 34);
+            this.lbl_Total.TabIndex = 5;
+            this.lbl_Total.Text = "Total";
+            
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(807, 258);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(256, 33);
+            this.comboBox1.TabIndex = 6;
+            
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(421, 258);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(256, 33);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(50, 258);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(256, 33);
+            this.comboBox3.TabIndex = 8;
+            
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(421, 457);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 35);
+            this.textBox1.TabIndex = 9;
+            
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(807, 457);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 35);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(50, 457);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(256, 35);
+            this.textBox4.TabIndex = 12;
+            
+            // 
+            // frm_Dependant_Combo_Box
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1099, 658);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_Total);
+            this.Controls.Add(this.lbl_CS);
+            this.Controls.Add(this.lbl_Quantity);
+            this.Controls.Add(this.lbl_Product);
+            this.Controls.Add(this.lbl_Subcategory);
+            this.Controls.Add(this.lbl_Category);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Dependant_Combo_Box";
+            this.Text = "Dependant Combo Box";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Category;
+        private System.Windows.Forms.Label lbl_Subcategory;
+        private System.Windows.Forms.Label lbl_Product;
+        private System.Windows.Forms.Label lbl_Quantity;
+        private System.Windows.Forms.Label lbl_CS;
+        private System.Windows.Forms.Label lbl_Total;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Assignments/Assignment_No8/Assignment_No8/Assignment_No8/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_No8
+{
+    public partial class frm_Dependant_Combo_Box : Form
+    {
+        public frm_Dependant_Combo_Box()
+        {
+            InitializeComponent();
+        }
+
+   } 
+}
